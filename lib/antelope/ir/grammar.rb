108f1d2ad5a77c76a029babd4139b8d743fc2f3a
Scott Taylor <scott@railsnewbie.com>
2009-10-16 23:47:33 -0400

    Better Grammar#compilable?

--- lib/antelope/ir/grammar.rb
+++ lib/antelope/ir/grammar.rb
@@ -4,15 +4,23 @@ module Antelope
       attr_reader :start_symbol
       
       def start_symbol=(sym_or_string)
-        @start_symbol = sym_or_string.to_sym
+        @start_symbol = sym_or_string.respond_to?(:to_sym) ? sym_or_string.to_sym : sym_or_string
       end
       
       def compilable?
-        start_symbol ? true : false
+        start_symbol && start_symbol_valid? && rules.any?
       end
       
       def rules
         @rules ||= []
+      end
+      
+      attr_writer :rules
+      
+    private
+    
+      def start_symbol_valid?
+        rules.any? { |r| r.name == start_symbol }
       end
     end
   end
