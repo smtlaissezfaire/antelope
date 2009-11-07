ff29e91621ec1fff0a20a30d003a6d9e4f1f4e58
Scott Taylor <scott@railsnewbie.com>
2009-11-07 12:59:26 -0500

    Split up grammar

--- lib/antelope/parser.rb
+++ lib/antelope/parser.rb
@@ -3,7 +3,11 @@ module Antelope
     extend Using
 
     require 'treetop'
+
+    Treetop.load File.dirname(__FILE__) + "/parser/tokens.treetop"
+    Treetop.load File.dirname(__FILE__) + "/parser/rules.treetop"
     Treetop.load File.dirname(__FILE__) + "/parser/treetop_parser.treetop"
+
     using :Instantiators
   end
 end
