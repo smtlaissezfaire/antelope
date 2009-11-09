631fa90bfb762470cfa67173c76a6fecb7289cd7
Scott Taylor <scott@railsnewbie.com>
2009-11-08 20:56:41 -0500

    Fix specs to get 100% coverage (thanks rcov)

--- spec/antelope/ir/optional_repetition_spec.rb
+++ spec/antelope/ir/optional_repetition_spec.rb
@@ -5,20 +5,20 @@ module Antelope
     describe OptionalRepetition do
       it "should allow for one expression" do
         one = Object.new
-        one_or_more = Repetition.new(one)
+        one_or_more = OptionalRepetition.new(one)
         one_or_more.expressions.should == [one]
       end
 
       it "should allow for two expressions" do
         one = Object.new
         two = Object.new
-        one_or_more = Repetition.new(one, two)
+        one_or_more = OptionalRepetition.new(one, two)
         one_or_more.expressions.should == [one, two]
       end
 
       it "should raise if none are given" do
         lambda {
-          Repetition.new
+          OptionalRepetition.new
         }.should raise_error(ArgumentError)
       end
     end
